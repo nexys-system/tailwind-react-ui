c04c4ed6884cdd276819cf18a003dc966ced9c47
John B <johan@nexys.ch>
2021-09-15 17:58:26 +0200

    Update icon-factory.tsx

--- src/lib/icons/icon-factory.tsx
+++ src/lib/icons/icon-factory.tsx
@@ -5,8 +5,15 @@ import Wrapper from "./svg-wrapper";
 // add React.SVGAttributes ?
 // see https://github.com/DefinitelyTyped/DefinitelyTyped/blob/1349b640d4d07f40aa7c1c6931f18e3fbf667f3a/types/react/index.d.ts#L2341
 
+type StrokeLine = "inherit" | "round" | "butt" | "square" | undefined;
+
 const IconFactorySVG =
-  (d: string, strokeWidth: number = 2, strokeLinecap:string = "round", strokeLinejoin:string = "round") =>
+  (
+    d: string,
+    strokeWidth: number = 2,
+    strokeLinecap: StrokeLine = "round",
+    strokeLinejoin: StrokeLine = "round"
+  ) =>
   ({ className, svgProps }: { className?: string, svgProps?: React.SVGAttributes<any> }) =>
     (
       <Wrapper className={className}>
