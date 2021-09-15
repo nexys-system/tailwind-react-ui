13d603a232bcb60db94f54ef9481f79d63f2b187
John B <johan@nexys.ch>
2021-09-15 17:48:59 +0200

    Update icon-factory.tsx

--- src/lib/icons/icon-factory.tsx
+++ src/lib/icons/icon-factory.tsx
@@ -6,13 +6,13 @@ import Wrapper from "./svg-wrapper";
 // see https://github.com/DefinitelyTyped/DefinitelyTyped/blob/1349b640d4d07f40aa7c1c6931f18e3fbf667f3a/types/react/index.d.ts#L2341
 
 const IconFactorySVG =
-  (d: string, strokeWidth: number = 2) =>
+  (d: string, strokeWidth: number = 2, strokeLinecap:string = "round", strokeLinejoin:string = "round") =>
   ({ className }: { className?: string }) =>
     (
       <Wrapper className={className}>
         <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
+          strokeLinecap={strokeLinecap}
+          strokeLinejoin={strokeLinejoin}
           strokeWidth={strokeWidth}
           d={d}
         />
