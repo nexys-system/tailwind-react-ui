c27a6c144614e40817ee99f3986972a47ed29278
John B <johan@nexys.ch>
2021-09-15 17:41:11 +0200

    Create index.tsx

---
+++ src/lib/icons/index.tsx
@@ -0,0 +1,13 @@
+import IconFactorySVG from "./icon-factory";
+
+export const CloudDownload = IconFactorySVG(
+  "M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10"
+);
+
+export const ArrowCircleRight = IconFactorySVG(
+  "M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+);
+
+export const XCircle = IconFactorySVG(
+  "M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+);
