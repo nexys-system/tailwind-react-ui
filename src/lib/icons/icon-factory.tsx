9400b27cc1e5ff12bb5a4cab0d6c796a7a560d58
John B <john@nexys.ch>
2021-09-16 11:44:55 +0200

    rm icons and replace with vars + script

--- src/lib/icons/icon-factory.tsx
+++ src/lib/icons/icon-factory.tsx
@@ -6,16 +6,22 @@ import Wrapper from "./svg-wrapper";
 // see https://github.com/DefinitelyTyped/DefinitelyTyped/blob/1349b640d4d07f40aa7c1c6931f18e3fbf667f3a/types/react/index.d.ts#L2341
 
 type StrokeLinecap = "inherit" | "round" | "butt" | "square" | undefined;
-type StrokeLinejoin = "inherit" | "round" | "miter" | "bevel" | undefined
+type StrokeLinejoin = "inherit" | "round" | "miter" | "bevel" | undefined;
 
 const IconFactorySVG =
   (
     d: string,
-    strokeWidth: number = 2,
+    strokeWidth: number,
     strokeLinecap: StrokeLinecap = "round",
     strokeLinejoin: StrokeLinejoin = "round"
   ) =>
-  ({ className, svgProps }: { className?: string, svgProps?: React.SVGAttributes<any> }) =>
+  ({
+    className,
+    svgProps,
+  }: {
+    className?: string;
+    svgProps?: React.SVGAttributes<any>;
+  }) =>
     (
       <Wrapper className={className}>
         <path
