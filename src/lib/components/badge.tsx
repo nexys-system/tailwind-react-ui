76de105f3037c688ce1d2115e82c548ec51cf890
John B <johan@nexys.ch>
2021-09-21 01:36:53 +0200

    Update badge.tsx

--- src/lib/components/badge.tsx
+++ src/lib/components/badge.tsx
@@ -1,7 +1,7 @@
 // inspired from https://getbootstrap.com/docs/4.0/components/badge/
 import React from "react";
 
-const Badge = ({ children, color = 'blue' }: { children: string; color?: string }) => (
+const Badge = ({ children, color = 'blue' }: { children: string | JSX.Element; color?: string }) => (
   <span
     className={`text-xs border rounded p-1 border-${color}-500 text-${color}-500  hover:border-${color}-800 hover:text-${color}-800`}
   >
