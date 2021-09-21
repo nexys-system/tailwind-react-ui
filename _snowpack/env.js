c15945951351ba92ac90c0d3ea662e8da83bf2ca
johnb8005 <johnb8005@users.noreply.github.com>
2021-09-21 16:12:24 +0000

    deploy: 6d0f8c4e983fbf728059945537c22f7adf5603ea

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,5 +1,5 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "bb1ac147200c4cc35bd5207cb96ee05968824b0c";
-export const SNOWPACK_PUBLIC_VERSION = "v1.0.23";
+export const SNOWPACK_PUBLIC_GIT_SHA = "6d0f8c4e983fbf728059945537c22f7adf5603ea";
+export const SNOWPACK_PUBLIC_VERSION = "v1.0.24";
 export const SNOWPACK_PUBLIC_URL = "/tailwind-react-ui";
 export const MODE = "production";
 export const NODE_ENV = "production";
--- app/form/switch.js
+++ app/form/switch.js
@@ -1,12 +1,13 @@
 import React from "../../_snowpack/pkg/react.js";
 import * as Inputs from "../../lib/form/inputs/index.js";
+import {FormType} from "../../lib/form/type.js";
 import Wrapper from "../../lib/form/wrapper.js";
 export default () => {
   const [value, setValue] = React.useState();
   return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("code", null, value), /* @__PURE__ */ React.createElement(Wrapper, {
-    inputType: "switch",
+    inputType: FormType.Switch,
     label: "Switch (boolean input)"
-  }, /* @__PURE__ */ React.createElement(Inputs.Switch, {
+  }, /* @__PURE__ */ React.createElement(Inputs.Boolean.Switch, {
     value,
     onChange: (v) => setValue(v)
   })));
