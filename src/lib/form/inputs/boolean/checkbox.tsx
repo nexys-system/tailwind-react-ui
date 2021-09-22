5b99be6637dc7610816f304d59b4a0e23776c6c8
John B <john@nexys.ch>
2021-09-22 12:51:57 +0200

    fix checkbox

--- src/lib/form/inputs/boolean/checkbox.tsx
+++ src/lib/form/inputs/boolean/checkbox.tsx
@@ -26,7 +26,7 @@ export default ({ value, disabled, onChange }: T.InputProps<boolean>) => {
         <input
           disabled={disabled}
           type="checkbox"
-          className="form-checkbox w-5 h-5 text-sky-500 dark:bg-coolGray-700 focus:outline-none focus:shadow-none cursor-pointer"
+          className="form-checkbox w-5 h-5 text-primary-500 dark:bg-coolGray-700 focus:ring-offset-0 focus:ring-0 focus:shadow-none focus:outline-none cursor-pointer"
           checked={value === undefined ? false : value}
           onChange={() => onChange(!value)}
         />
