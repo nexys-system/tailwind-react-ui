e73e74d96db20076718a33a2fa79f1c9921445db
johnb8005 <johnb8005@users.noreply.github.com>
2021-09-22 10:33:27 +0000

    deploy: 142705b51fe385fb2d73a3255eaa49ef9aac8ff8

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,5 +1,5 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "6ae24b87882eafe14061217108dc017ec6e1ccee";
-export const SNOWPACK_PUBLIC_VERSION = "v1.0.28";
+export const SNOWPACK_PUBLIC_GIT_SHA = "142705b51fe385fb2d73a3255eaa49ef9aac8ff8";
+export const SNOWPACK_PUBLIC_VERSION = "v1.0.29";
 export const SNOWPACK_PUBLIC_URL = "/tailwind-react-ui";
 export const MODE = "production";
 export const NODE_ENV = "production";
