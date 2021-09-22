34855ea964bef3f68bb22ee54e461920a63291e1
John B <john@nexys.ch>
2021-09-22 13:41:43 +0200

    card

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,12 +1,47 @@
 import React from "react";
+import Typography from "../../lib/components/typography";
 import Card from "../../lib/components/card";
+import { Sun as Icon, Edit as EditIcon } from "../../lib/icons";
+
+const Right = () => {
+  const [editShow, setEditShow] = React.useState<boolean>(false);
+  const [sunShow, setSunShow] = React.useState<boolean>(false);
+
+  return (
+    <span className={"text-sm"}>
+      <a
+        href="#"
+        onMouseOver={() => setEditShow(true)}
+        onMouseOut={() => setEditShow(false)}
+      >
+        <EditIcon />
+        {editShow && <>Edit</>}
+      </a>
+      <a
+        href="#"
+        onMouseOver={() => setSunShow(true)}
+        onMouseOut={() => setSunShow(false)}
+      >
+        <Icon /> {sunShow && <>Sun</>}
+      </a>
+    </span>
+  );
+};
 
 export default () => {
   return (
     <>
-      <h1>Card</h1>
+      <Typography variant={"h2"}>Card</Typography>
       <Card title="Card Title">
         <p>Content</p>
+      </Card>
+
+      <Card>
+        <p>no title</p>
+      </Card>
+
+      <Card right={<Right />}>
+        <p>no title</p>
       </Card>
     </>
   );
--- src/lib/components/card.tsx
+++ src/lib/components/card.tsx
@@ -5,26 +5,27 @@ const Card = ({
   title,
   style,
   className,
+  right,
 }: {
   title?: string;
   children: JSX.Element | JSX.Element[];
   style?: React.CSSProperties;
   className?: string;
+  right?: JSX.Element | JSX.Element[];
 }) => {
   return (
     <div
       style={style}
-      className={`w-full bg-white dark:bg-coolGray-600 dark:text-white rounded-xl shadow-md ${className}`}
+      className={`w-full bg-white dark:bg-coolGray-600 dark:text-white rounded-xl shadow-md ${className} m-1`}
     >
-      <div className="">
-        <div className="p-8">
-          {title && (
-            <div className="uppercase tracking-wide text-sm text-primary font-semibold">
-              {title}
-            </div>
-          )}
-          {children}
-        </div>
+      {right && <div className={"float-right pr-4 pt-4"}>{right}</div>}
+      <div className="p-4">
+        {title && (
+          <div className="uppercase tracking-wide text-sm text-primary font-semibold">
+            {title}
+          </div>
+        )}
+        {children}
       </div>
     </div>
   );
