732939497e5a492bbd4ca069d50af3bfcd706e43
John B <john@nexys.ch>
2021-12-17 00:04:20 +0100

    rm form toggle

--- src/lib/components/form/index.ts
+++ src/lib/components/form/index.ts
@@ -2,7 +2,3 @@ export * as Input from "./input";
 export * as Wrapper from "./wrapper";
 
 export * as Select from "./input/select";
-import Toggle from "./toggle";
-//import Form from "./form";
-
-export { Toggle };
